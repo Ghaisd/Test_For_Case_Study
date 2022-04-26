545fcfbc596092ef0f57f7d598e3fbaaa485db9c
Ghias Dahdouh <ghaisdahdouh@gmail.com>
2022-04-26 15:14:17 +0200

    added the answer for add method

--- Test_Case_For_Case_Study/Test_Case_Fro_Case_Study/Program.cs
+++ Test_Case_For_Case_Study/Test_Case_Fro_Case_Study/Program.cs
@@ -1,3 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+Console.WriteLine(add(1,2));
+
+static int add(int x, int y)
+{
+    return x + y;
+}
